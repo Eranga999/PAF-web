5a7fea55853a87c539f0e67339e8de0a76f88ddc
eranga999 <it22253644@my.sliit.lk>
2025-03-30 00:37:20 +0530

    Update App.jsx

--- frontend/tailwindcss4/src/App.jsx
+++ frontend/tailwindcss4/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-
+//learning planpages
 import CulinaryJourneyPage from "./pages/learningplan/CulinaryJourneyPage";
 import CommunityExplorePage from  "./pages/learningplan/CommunityExplorePage";
 import LearningPlanPage from    "./pages/learningplan/LearningPlanPage";
@@ -12,7 +11,7 @@ function App() {
     <Routes>
 
       <Route path="/" element={<CulinaryJourneyPage />} />
-      
+
       <Route path="/community" element={<CommunityExplorePage />} />
       <Route path="/learningplan" element={<LearningPlanPage />} />
     </Routes>
