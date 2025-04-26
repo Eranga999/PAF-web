823c0ba46890c6dfb6fa4214765efc7208a8a1e0
Your Name <yasadinethmini>
2025-04-26 15:10:58 +0530

    Delete Image.java

--- cookmate/src/main/java/com/skillshare/cooking/repository/Image.java
+++
@@ -1,7 +0,0 @@
-package com.skillshare.cooking.repository;
-
-import com.skillshare.cooking.entity.Image;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface Image extends MongoRepository<Image, String> {
-}
